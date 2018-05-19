1ff2f84a03dff5f0b2aaa27ccfc0f9e554920a0a
ghCarol <carol351765753@163.com>
2018-05-19 16:44:56 +0800

    2048小游戏完成
    滑动动画未完成

---
+++ utils/util.js
@@ -0,0 +1,22 @@
+//用于格式化时间
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n //将日期如“5”号格式化为“05”，js数组越界返回undefined，可用三元表达式确认
+}
+
+module.exports = {
+  formatTime: formatTime
+}
